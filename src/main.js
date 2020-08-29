1dab67a9d9a8b761242ef57eaa444e7c6c094978
YSGRST <YSG8975@163.com>
2020-08-29 10:20:07 +0800

    携程

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+// 配置每一个组件可以使用vue-router
+Vue.use(VueRouter)
+import Mint from 'mint-ui';
+import '../node_modules/mint-ui/lib/style.css'
+Vue.use(Mint);
+import './assets/css/font_2025156_zvrpfrga01f/iconfont.css'
+import './assets/css/font_2026272_b4xosajrjut/iconfont.css'
+import './assets/css/font_2028470_139jx82skvw/iconfont.css'
+import './assets/css/font_2024487_2ix7pe88ly3/iconfont.css'
+import './assets/css/shi/iconfont.css'
+import './assets/css/cssreset.css'
+// 引入路由
+import router from '../src/assets/js/router.js'
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
